675307d0f5eb04c9520cf2b7cace99db7eee23f6
cwilson93 <cwilson2@chicagobooth.edu>
2022-10-27 20:35:48 +0000

    Gittttt

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,12 @@
+class DirectorsController < ApplicationController
+  def index
+    @list_of_directors = Director.all
+
+    render({ :template => "directors_template/index.html.erb"})
+  end
+
+
+
+
+
+end
