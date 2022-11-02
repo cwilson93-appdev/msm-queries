190e5e1805078c8be1ea64c8a587c47d74a0632d
cwilson93 <cwilson2@chicagobooth.edu>
2022-11-02 22:11:14 +0000

    end of video

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,7 +5,25 @@ class DirectorsController < ApplicationController
     render({ :template => "directors_template/index.html.erb"})
   end
 
+  def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc}).at(0)
 
+    render({ :template => "directors_template/eldest.html.erb"})
+  end
+
+  def dumbest
+    @youngest = Director.order({ :dob => :desc}).at(0)
+
+    render({ :template => "directors_template/youngest.html.erb"})
+  end
+
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({ :id => the_id}).at(0)
+    @filmography = Movie.where({ :director_id => @the_director.id })
+
+    render({ :template => "directors_template/show.html.erb"})
+  end
 
 
 
